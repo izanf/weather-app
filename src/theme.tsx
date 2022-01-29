83c00895b02e203a7eb762db6edbffa23e85f69c
Izanderson Florencio <izandersonf@gmail.com>
2022-01-29 19:35:21 -0300

    feat: project theming

---
+++ src/theme.tsx
@@ -0,0 +1,40 @@
+const theme = {
+  colors: {
+    transparent: 'transparent',
+    white: '#FFFFFF',
+    black: '#000000',
+    primary: '#100E1D',
+    primaryLight: '#1E213A',
+    gray: '#6E707A',
+    grayLight: '#E7E7EB',
+    grayDark: '#A09FB1',
+    grayDarker: '#88869D',
+    blue: '#3C47E9'
+  },
+  borderRadius: {
+    xsmall: '.25rem',
+    small: '.5rem',
+    medium: '1rem',
+    large: '2rem',
+    xlarge: '3rem',
+  },
+  buttonSize: {
+    small: {
+      padding: '.5rem 1rem',
+      fontSize: '1rem',
+    },
+    medium: {
+      padding: '1rem 1.5rem',
+      fontSize: '1.2rem',
+    },
+  },
+  breakpoints: {
+    sm: '576px',
+    md: '768px',
+    lg: '992',
+    xl: '1200px',
+    xxl: '1400px',
+  },
+};
+
+export default theme;
